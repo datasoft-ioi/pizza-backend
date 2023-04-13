bc10beba1c850e83ef381f35acbb167837cfc579
Satipov Akbar <88106359+codewithakbar@users.noreply.github.com>
2023-04-13 18:37:29 +0500

    url

--- pizza/urls.py
+++ pizza/urls.py
@@ -23,8 +23,8 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-
-    path('api/v1/', include('products.urls', namespace="products")),
+    path('', include('products.urls', namespace="products")),
+    # path('api/v1/', include('products.urls', namespace="products")),
     path('users/', include('users.urls', namespace="users")),
 
 
